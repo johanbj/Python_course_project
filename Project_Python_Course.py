414f13889a37fb61fff2a2ac4e24cb6f54bf0d29
Johan Björkeroth <johanbj@chalmers.se>
2017-11-27 16:27:54 +0100

    Added code to enable read of csv files

--- Project_Python_Course.py
+++ Project_Python_Course.py
@@ -1,2 +1,16 @@
 # Python code that is meant as my program, in the python course I am currently taking
 
+# This program takes a list of Protein names, as well as their abundancies.
+# It then compares protein levels, between different samples.
+
+# The program should have an option to choose between analysing simulation data or experimental data
+# This could for example tell the program if normalization is required or not.
+
+file = '/Users/johanbj/Documents/Courses/Python/protein_test.csv'
+
+import csv
+with open(file, newline='') as csvfile:
+	spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+	for row in spamreader:
+		print(row[0])
+		
