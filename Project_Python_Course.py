f79f077609634ec41737148edac7341c18e1f591
Johan Björkeroth <johanbj@chalmers.se>
2017-12-17 23:40:49 +0100

    Updated the script so that it now can get all the unique pathway names in one list

--- Project_Python_Course.py
+++ Project_Python_Course.py
@@ -17,27 +17,17 @@ collection_of_pathways = []
 
 for line in protein_pathways:     # Go through all lines in the text-file
 	line = line.rstrip()          # Get rid of the new-line character.
-	list_from_line = line.split(' sce0')
+	list_from_line = line.split(' sce0')     # Split and categorize the line, based on a unique identifier
 
-#############################################
-
-# I NEED TO ADD THE 'SCE' TO THE ELEMENT AGAIN
-
-#############################################
+	if listlen>1:                       # If the list now has more than 1 element, all elements except
+		for j in range(1,(listlen)):    # the first must have 'sce0' added to it again.
+			list_from_line[j] = 'sce0' + list_from_line[j]
 	
-	collection_of_pathways.extend(list_from_line)
-print(collection_of_pathways)
-print(len(collection_of_pathways))
+	collection_of_pathways.extend(list_from_line)    # Collect all split pathway names
 
 unique_pathways = []
-for pathway_code in collection_of_pathways:
-    if pathway_code not in unique_pathways:
-        unique_pathways.append(pathway_code)
-print(unique_pathways)
-print(len(unique_pathways))
-	#for pathway in list_from_line:
-	#	if pathway
-	#print(repr(line))
-	#print(repr(line.rstrip()))
+for pathway_name in collection_of_pathways:
+    if pathway_name not in unique_pathways:     # Filter so that all names will be unique in the list
+        unique_pathways.append(pathway_name)
 
-
+protein_pathways.close()     # Close the file
